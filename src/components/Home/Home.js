2ce87021e77d03f92ca83c371d44857222bdf2cd
mbusiseni Mbhele <mbusiseni.mbhele@gmail.com>
2022-03-26 16:37:57 +0200

    added commit

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -22,7 +22,7 @@ function Home() {
                 <strong className="main-name"> Mbusiseni Mbhele</strong>
               </h1>
 
-              <div style={{ padding: 51, textAlign: "left" }}>
+              <div style={{ padding: 50, textAlign: "left" }}>
                 <Type />
               </div>
             </Col>
