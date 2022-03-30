6506de4eff83f71372f6ccc4ea4eddd521ca9df2
mbusiseni Mbhele <mbusiseni.mbhele@gmail.com>
2022-03-30 09:02:28 +0200

    applying grammar

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
-import { ImPointRight } from "react-icons/im";
 // import { FaBeer } from 'react-icons/fa';
 import { FaCheck} from 'react-icons/fa';
 
@@ -9,21 +8,21 @@ function AboutCard() {
     <Card className="quote-card-view">
       <Card.Body>
         <blockquote className="blockquote mb-0">
-          <p style={{ textAlign: "justify" }}>
-           Hello again, I am <span className="purple">Mbusiseni Mbhele </span>
-           junior developer from <span className="purple"> South Africa, Stellenbosch. </span>
-            <br />I am innovative, task driven professional with 3+ years of experience in web design and development across  I am passionate about JavaScript or any web technologies. I work extremely hard, and I am willing to learn new trending tech stacks. 
-            I value communication and writing clean and readable code
+        <p style={{ textAlign: "justify" }}>
+          Hello, My name is<span className="purple">  Mbusiseni Mbhele </span>, and I am a developer and entrepreneur based in South Africa (Stellenbosch).
+        I am an innovative, task-driven professional with <span className="purple">3+ years of experience in web design and development</span> . I am passionate about JavaScript and its great libraries such: as Dojo Toolkit, Reactjs, Nextjs and TensorFlow. I work extremely hard, and I am willing to learn new trending tech stacks. 
+            I value communication and writing clean and readable code. My job has become my vocation, and I still like to pass on this strength.
             <br />
             <br />
             Apart from coding, some other activities that I love to do!
           </p>
+
           <ul>
             <li className="about-activity">
-              <FaCheck />  Meditation
+              <FaCheck />  Gym
             </li>
             <li className="about-activity">
-              <FaCheck  /> Entrepreneur
+              <FaCheck  /> volunteering 
             </li>
             <li className="about-activity">
             <FaCheck /> Travelling
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -3,8 +3,6 @@ import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
 import acutesolar from "../../Assets/Projects/acutesolar.png";
-import emotion from "../../Assets/Projects/emotion.jpeg";
-import editor from "../../Assets/Projects/codeEditor.png";
 import gb from "../../Assets/Projects/gb.png";
 
 import easylandlord from "../../Assets/Projects/easylandlord.png";
@@ -26,8 +24,8 @@ function Projects() {
               imgPath={gb}
               isBlog={false}
               title="matshamatics.co.za"
-              description="This tutoring website built for for Dr Matsha who a luecture at num. The basic the website display the server doctor is rendering and the contact
-               It boostarct website hosted on the Google cloud firebase and it domain point from go daya"
+              description="This tutoring website was built for Dr Matsha. The website allow a learner to book tutoring lessons with easy. It uses css , html , javascript and google firebase for backend.
+               "
               link="http://matshamatics.co.za/"
             />
           </Col>
@@ -37,7 +35,7 @@ function Projects() {
               imgPath={acutesolar}
               isBlog={false}
               title="AcuteSolar"
-              description="This website was built for the solar entiety. It a simple website built with javascript , css, html, boostrap and php tech stack. The aim of the entitity was to display it services and contact details to it customers "
+              description="The website is for the solar panel entity. It a simple website built with in javascript , css, html, and boostrap. The aim of the entitity is to display it services and contact details to it customers. "
               link="https://acutesolar.co.za/"
             />
           </Col>
@@ -46,9 +44,9 @@ function Projects() {
             <ProjectCard
               imgPath={easylandlord }
               isBlog={false}
-              title=""
-              description=""
-              link=""
+              title="easylandlords"
+              description="This is a full-stack application designed for landlords who aims at displaying their services online. It also helps landlords study other landlord selling propositions on the website."
+              link="https://github.com/mbhele/easylandlords/"
             />
           </Col>
 
